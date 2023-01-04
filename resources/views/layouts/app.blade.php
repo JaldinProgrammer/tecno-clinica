243729856156eec8b426bab806367332d70742e0
fernanCayoC <fernandocayolacar+gmail@gmail.com>
2023-01-03 21:49:16 -0400

    gaaa

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,14 +7,21 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>CEMM</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <!-- <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet"> -->
+
+    
+    <link rel="shortcut icon" href="assets/img/logo.png" type="image/x-icon">
+
+    <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
+
+    <link rel="stylesheet" href="../../../css/styles.css">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
@@ -22,45 +29,47 @@
 </head>
 <body class="d-flex flex-column min-vh-100">
 <div id="app">
-    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-        <div class="container">
-            <a class="navbar-brand" href="{{ url('/') }}">
-                {{ config('app.name', 'Laravel') }}
+<header class="header" id="header">
+    <nav class=" nav container ">
+            <a class="nav__logo" href="{{ url('/') }}">
+            <img src="../../images/logoo.png" alt="" class="nav__logo-img">
+                CEMM
             </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+            <!-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                 <span class="navbar-toggler-icon"></span>
-            </button>
+            </button> -->
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <!-- <div class="collapse navbar-collapse" id="navbarSupportedContent"> -->
                 <!-- Left Side Of Navbar -->
-                <ul class="navbar-nav me-auto">
+                <!-- <ul class="navbar-nav me-auto">
 
-                </ul>
+                </ul> -->
 
                 <!-- Right Side Of Navbar -->
-                <ul class="navbar-nav ms-auto">
+                <div class="nav__menu" id="nav-menu">
+                <ul class="nav__list">
                     <!-- Authentication Links -->
                     @guest
                         @if (Route::has('login'))
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">Login</a>
+                            <li class="nav__item">
+                                <a class="nav__link" href="{{ route('login') }}">Login</a>
                             </li>
                         @endif
 
                         @if (Route::has('register'))
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('register') }}">Registrar</a>
+                            <li class="nav__item">
+                                <a class="nav__link" href="{{ route('register') }}">Registrar</a>
                             </li>
                         @endif
                     @else
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('user.index') }}">Usuarios</a>
+                        <li class="nav__item">
+                            <a class="nav__link" href="{{ route('user.index') }}">Usuarios</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('diagnostic.index') }}">Diagnostico</a>
+                        <li class="nav__item">
+                            <a class="nav__link" href="{{ route('diagnostic.index') }}">Diagnostico</a>
                         </li>
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        <li class="nav__item dropdown">
+                            <a id="navbarDropdown" class="nav__link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                 {{ Auth::user()->name }}
                             </a>
 
@@ -77,16 +86,26 @@
                             </div>
                         </li>
                     @endguest
+
+                    <div class="nav__close" id="nav-close">
+                        <i class="bx bx-x"></i>
+                    </div>
                 </ul>
             </div>
-        </div>
+            <div class="nav_btns">
+                    <i class="bx bx-moon change-theme" id="theme-button"></i>
+                    <div class="nav__toggle" id="nav-toggle">
+                        <i class="bx bx-grid-alt"></i>
+                    </div>
+                </div>
+            </div>
     </nav>
+    </header>
 
     <main class="py-4">
         @yield('content')
     </main>
 </div>
-@yield('js')
-@extends('layouts.footer')
+<script src="../js/main.js"></script>
 </body>
 </html>
